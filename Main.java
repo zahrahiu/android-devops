54771a69442178141d6d461134992f262e01e726
simoks <mohamed.kouissi@gmail.com>
2024-03-26 10:20:56 +0000

    add V3 main java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello DevOps V2 !!!");
+    public static void main(String[] args) {
+        System.out.println("Hello DevOps V3 !!!");
     }
 }
